2a8b3a4b027f4d942dd2b1e3f54b7224e2351f2b
Harsh <harshpatel071@gmail.com>
2021-01-31 00:25:41 -0600

    Server skeleton implemented with a successful connection

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+// IMPORT PACKAGES.
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose'); // Connects to mongodb.
+
+require('dotenv').config();
+
+//Create express server.
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Parse JSON ( SENDS AND RECEIVE JSON )
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
