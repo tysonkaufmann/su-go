3e41812c5baace9fa8a9e678756de73254eb7c65
Harsh <harshpatel071@gmail.com>
2021-01-31 01:33:34 -0600

    Added simple user routes containing a simple userschema

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,9 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+// USER ROUTE
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
