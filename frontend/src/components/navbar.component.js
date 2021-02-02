c267cc99bb08c2803f08548d832e57e881f68b08
Cheng <oofhb601@gmail.com>
2021-02-02 16:34:44 -0600

    Correct attribute names in frontend - navbar

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -6,20 +6,20 @@ export default class Navbar extends Component {
 
   render() {
     return (
-      <nav class="navbar navbar-expand-md navbar-light">
-        <div class="navbar-collapse collapse ">
-            <ul class="navbar-nav ms-auto">
-                <li class="nav-item active">
+      <nav className="navbar navbar-expand-md navbar-light">
+        <div className="navbar-collapse collapse ">
+            <ul className="navbar-nav ms-auto">
+                <li className="nav-item active">
                   <Link to="/" className="nav-link">Home</Link>
                 </li>
             </ul>
         </div>
-        <div class="mx-auto">
+        <div className="mx-auto">
           <img src={Logo} width="40" height="40" alt="" />
           <Link to="/" className="navbar-brand mx-auto">&nbsp;Su;Go</Link>
         </div>
-        <div class="navbar-collapse collapse">
-            <ul class="navbar-nav ml-auto">
+        <div className="navbar-collapse collapse">
+            <ul className="navbar-nav ml-auto">
               <li className="navbar-item">
                 <Link to="/login" className="nav-link">Log In</Link>
               </li>
