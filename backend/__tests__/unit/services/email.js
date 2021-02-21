4f32cb97deb0462543900f4f22ce3b6fe38d127b
tysonkaufmann <umkaufmt@myumanitoba.ca>
2021-02-20 22:31:02 -0600

    Add console logs to email test

--- backend/__tests__/unit/services/email.js
+++ backend/__tests__/unit/services/email.js
@@ -6,8 +6,11 @@ describe("EmailService", () => {
   it('Email Sending Successful - Correct Email', async done => {
     // Sends email
     jest.setTimeout(30000);
+    console.log("Creating Email object")
     var emailService = new Email();
+    console.log("Sending Email")
     const response = await emailService.send("mitul0011@gmail.com")
+    console.log("Email sent - checking response")
     expect(typeof response).toBe('string');
     done()
   })
